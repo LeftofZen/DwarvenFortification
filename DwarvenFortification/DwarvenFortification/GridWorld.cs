bc6b960878fdee35988feb216ff71f32416ac4b4
Benjamin Sutas <benjamin.sutas@wisetechglobal.com>
2021-04-28 18:01:23 +1000

    attempts to add content

---
+++ DwarvenFortification/DwarvenFortification/GridWorld.cs
@@ -0,0 +1,29 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using Microsoft.Xna.Framework;
+//using Microsoft.Xna.Framework.Content;
+//using MonoGame.Extended.Tiled;
+//using MonoGame.Extended.Tiled.Renderers;
+
+//namespace DwarvenFortification
+//{
+//	public class GridWorld
+//	{
+//		TiledMap map;
+//		TiledMapRenderer mapRenderer;
+
+//		public GridWorld (int width, int height)
+//		{
+
+//		}
+//		protected void LoadContent(ContentManager content)
+//		{
+//			map = content.Load<TiledMap>("samplemap");
+//			mapRenderer = new TiledMapRenderer(GraphicsDevice, _tiledMap);
+
+//			_spriteBatch = new SpriteBatch(GraphicsDevice);
+//		}
+
+//	}
+//}
--- DwarvenFortification/DwarvenFortification/MainGame.cs
+++ DwarvenFortification/DwarvenFortification/MainGame.cs
@@ -1,15 +1,19 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Renderers;
 
 namespace DwarvenFortification
 {
-	public class Game1 : Game
+	public class MainGame : Game
 	{
 		private GraphicsDeviceManager _graphics;
 		private SpriteBatch _spriteBatch;
+		TiledMap _tiledMap;
+		TiledMapRenderer _tiledMapRenderer;
 
-		public Game1()
+		public MainGame()
 		{
 			_graphics = new GraphicsDeviceManager(this);
 			Content.RootDirectory = "Content";
@@ -25,17 +29,20 @@ namespace DwarvenFortification
 
 		protected override void LoadContent()
 		{
-			_spriteBatch = new SpriteBatch(GraphicsDevice);
+			_tiledMap = Content.Load<TiledMap>("tiles//samplemap");
+			_tiledMapRenderer = new TiledMapRenderer(GraphicsDevice, _tiledMap);
 
-			// TODO: use this.Content to load your game content here
+			_spriteBatch = new SpriteBatch(GraphicsDevice);
 		}
 
 		protected override void Update(GameTime gameTime)
 		{
 			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+			{
 				Exit();
+			}
 
-			// TODO: Add your update logic here
+			_tiledMapRenderer.Update(gameTime);
 
 			base.Update(gameTime);
 		}
@@ -44,7 +51,7 @@ namespace DwarvenFortification
 		{
 			GraphicsDevice.Clear(Color.CornflowerBlue);
 
-			// TODO: Add your drawing code here
+			_tiledMapRenderer.Draw();
 
 			base.Draw(gameTime);
 		}
