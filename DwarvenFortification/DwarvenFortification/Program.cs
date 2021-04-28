779636b8a99bd89481347b093012bbf3eeda230c
Benjamin Sutas <benjamin.sutas@wisetechglobal.com>
2021-04-28 17:05:30 +1000

    initial solution commit

---
+++ DwarvenFortification/DwarvenFortification/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DwarvenFortification
+{
+	public static class Program
+	{
+		[STAThread]
+		static void Main()
+		{
+			using (var game = new Game1())
+				game.Run();
+		}
+	}
+}
